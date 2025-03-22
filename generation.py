d0eaafa63096be2dc1f47c0128e81884b4ba58c8
root <root@ubuntu22.04>
2025-03-22 22:58:10 +0800

    initialize gpt

--- generation.py
+++ generation.py
@@ -1,16 +1,18 @@
 import torch
 import torch.nn.functional as F
 from model import GPT, GPTConfig
+import time
 
 device = "cpu"
+torch.manual_seed(42)
 if torch.cuda.is_available():
     device = "cuda"
+    torch.cuda.manual_seed(42)
 elif torch.mps.is_available() and hasattr(torch.backends, "mps"):
     device = "mps"
+    torch.mps.manual_seed(42)
 print("device:", device)
-torch.manual_seed(42)
-torch.cuda.manual_seed(42)
-torch.mps.manual_seed(42)
+
 
 # # prefix tokens
 import tiktoken
@@ -31,7 +33,8 @@ enc = tiktoken.get_encoding('gpt2')
 # y = buf[1:].view(B, T).to(device)
 
 from dataloader import DataLoaderLite
-train_loader = DataLoaderLite(4, 32)
+train_loader = DataLoaderLite(B=16, T=1024)
+torch.set_float32_matmul_preision('high')
 
 # model = GPT.from_pretrained('gpt2')
 model = GPT(GPTConfig())
@@ -40,13 +43,18 @@ model.to(device)
 # optimize
 optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
 for i in range(50):
+    t0 = time.time()
     x, y = train_loader.next_batch()
     x, y = x.to(device), y.to(device)
     optimizer.zero_grad()
     logits, loss = model(x, y)
     loss.backward()
     optimizer.step()
-    print(f"step {i}, loss: {loss.item()}")
+    torch.cuda.synchronize() # wait for the GPU finish work
+    t1 = time.time()
+    dt = (t1-t0)*1000
+    tokens_per_sec = (train_loader.B * train_loader.T) / (t1-t0)
+    print(f"step {i}, loss: {loss.item()}, dt: (dt:.2f)ms, tok/sec:{tokens_per_sec:.2f}")
 
 import sys; sys.exit(0)
 
--- model.py
+++ model.py
@@ -21,6 +21,7 @@ class CausalSelfAttention(nn.Module):
         self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
         # output projection
         self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        self.c_proj.RESIDUAL_SCALE_INIT = 1
         # regularization
         self.n_head = config.n_head
         self.n_embd = config.n_embd
@@ -54,6 +55,7 @@ class MLP(nn.Module):
         self.c_fc = nn.Linear(config.n_embd, config.n_embd * 4)
         self.gelu = nn.GELU(approximate='tanh')
         self.c_proj = nn.Linear(config.n_embd * 4, config.n_embd)
+        self.c_proj.RESIDUAL_SCALE_INIT = 1
 
     def forward(self, x):
         x = self.c_fc(x)
@@ -90,6 +92,21 @@ class GPT(nn.Module):
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False) # output embedding
 
         self.transformer.wte.weight = self.lm_head.weight # tie weights of input and output embeddings
+        self.transformer.wpe.POSITION_SCALE_INIT = 1
+    
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            std = 0.02
+            if hasattr(module, 'RESIDUAL_SCALE_INIT'):
+                std *= (2 * self.config.n_layer) ** -0.5
+            torch.nn.init.normal_(module.weight, mean=0.0, std=std)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            std = 0.02
+            if hasattr(module, 'POSITION_SCALE_INIT'):
+                std = 0.01
+            torch.nn.init.normal_(module.weight, mean=0.0, std=std)
 
     def forward(self, idx, targets=None):
         B, T = idx.size()
